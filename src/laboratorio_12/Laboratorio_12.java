093f611f2232c4084f895ff5244ffe9b479cab8e
UrielParedes05 <142622188+UrielParedes05@users.noreply.github.com>
2023-11-03 23:20:52 -0500

    Add files via upload

---
+++ src/laboratorio_12/Laboratorio_12.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio_12;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Laboratorio_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Principal principal = new Principal();
+        
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+        
+    }
+    
+}
